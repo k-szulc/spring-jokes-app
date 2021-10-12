1b2b5585f36281cd5f2711afa070be238efcba4a
k-szulc <s-q-n@protonmail.com>
2021-10-12 17:01:44 +0200

    removed unused import, removed typo from gitignore

--- src/main/java/xyz/itbs/jokes/controllers/JokesController.java
+++ src/main/java/xyz/itbs/jokes/controllers/JokesController.java
@@ -1,6 +1,5 @@
 package xyz.itbs.jokes.controllers;
 
-import guru.springframework.norris.chuck.ChuckNorrisQuotes;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
